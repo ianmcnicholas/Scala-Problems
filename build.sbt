57ad278b13ca159b025174b8a12f35ce961a619b
ianmcnicholas <ianjmcnicholas@.com>
2023-09-13 09:20:58 +0100

    initial test setup

--- build.sbt
+++ build.sbt
@@ -5,6 +5,6 @@ version := "0.1"
 scalaVersion := "2.13.12"
 
 libraryDependencies ++= Seq(
-  "org.scalatest" %% "scalatest" % "3.2.7",
-  "org.scalatest" %% "scalatest" % "3.2.7" % "test"
+  "org.scalatest" %% "scalatest" % "3.2.15",
+  "org.scalatest" %% "scalatest" % "3.2.15" % "test"
 )
--- src/test/scala/ScalaTestingStyles.scala
+++ src/test/scala/ScalaTestingStyles.scala
@@ -1,3 +1,31 @@
-class ScalaTestingStyles {
+import org.scalatest.funsuite.AnyFunSuite
 
+object ScalaTestingStyles
+
+class CalculatorSuite extends AnyFunSuite {
+  val calculator = new Calculator
+
+  test("multiplication by 0 should always be 0"){
+    assert(calculator.multiply(4312, 0) == 0)
+    assert(calculator.multiply(-4312, 0) == 0)
+    assert(calculator.multiply(0, 0) == 0)
+    assert(calculator.multiply(0, 1) == 0)
+    assert(calculator.multiply(0, -1) == 0)
+  }
+
+  test("dividing by 0 should throw an error"){
+    assertThrows[ArithmeticException](calculator.divide(5, 0))
+
+  }
+
+}
+
+class Calculator {
+  def add(a: Int, b: Int): Int = a + b
+
+  def subtract(a: Int, b: Int): Int = a - b
+
+  def multiply(a: Int, b: Int): Int = a * b
+
+  def divide(a: Int, b: Int): Int = a / b
 }
